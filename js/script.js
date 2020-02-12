3d8be26c50803d2a7dcb4eb974d9dead2e8df7e5
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-12 15:20:41 +0600

    update stylesheet

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// Selecting form elements
 const form              = document.getElementById("form");
 const username          = document.getElementById("username");
 const email             = document.getElementById("email");
 const password          = document.getElementById("password");
 const confirmPassword   = document.getElementById("confirmPassword");
+
+
